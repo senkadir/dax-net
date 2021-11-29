f2d4ecbb9a1c46fbcdd5036fe88b2a181e8ef495
Abdulkadir Şen <abdulkadir.sen.external@cpp.canon>
2021-11-29 22:07:25 +0100

    Initial create

---
+++ src/Builder/DaxQuery.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Builder
+{
+    public class DaxQuery
+    {
+        public List<EvaluateQuery> Evaluates { get; set; }
+    }
+
+    public interface IQueryBuilder
+    {
+        IQueryBuilder Evaluate(Action<EvaluateBuilder> evaluateBuilder);
+
+        string Build();
+
+        string BuildRaw();
+    }
+
+    public abstract class QueryBuilderBase
+    {
+    }
+
+    public class QueryBuilder : QueryBuilderBase, IQueryBuilder
+    {
+        private DaxQuery query;
+
+        public QueryBuilder()
+        {
+            query = new DaxQuery
+            {
+                Evaluates = new()
+            };
+        }
+
+        public IQueryBuilder Evaluate(Action<EvaluateBuilder> evaluateBuilder)
+        {
+            EvaluateBuilder evaluateBuilderInner = new();
+
+            evaluateBuilder(evaluateBuilderInner);
+
+            query.Evaluates.Add(evaluateBuilderInner.Build());
+
+            return this;
+        }
+
+        public string Build()
+        {
+            StringBuilder builder = new();
+
+            foreach (var evaluate in query.Evaluates)
+            {
+                builder.AppendLine("EVALUATE(")
+                       .AppendLine($"'{evaluate.Table}'")
+                       .AppendLine(")");
+
+                if (evaluate.Orders.Any())
+                {
+                    builder.AppendLine($" ORDER BY ");
+
+                    builder.AppendJoin($",{Environment.NewLine} ", evaluate.Orders);
+                }
+
+                if (evaluate.Orders.Any() && evaluate.StartAts.Any())
+                {
+                    builder.AppendLine($" START AT ");
+
+                    builder.AppendJoin($",{Environment.NewLine} ", evaluate.StartAts);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public string BuildRaw()
+        {
+            return Build().Replace(Environment.NewLine, string.Empty);
+        }
+    }
+
+    public class EvaluateBuilder : QueryBuilderBase
+    {
+        EvaluateQuery _evaluate;
+
+        public EvaluateBuilder()
+        {
+            _evaluate = new();
+
+            _evaluate.Orders = new();
+            _evaluate.StartAts = new();
+        }
+
+        public EvaluateBuilder Table(string table)
+        {
+            _evaluate.Table = table;
+
+            return this;
+        }
+
+        internal EvaluateQuery Build()
+        {
+            return _evaluate;
+        }
+
+        public EvaluateBuilder OrderBy(string expression)
+        {
+            _evaluate.Orders.Add($"'{_evaluate.Table}'[{expression}] ASC");
+
+            return this;
+        }
+
+        public EvaluateBuilder OrderByDescending(string expression)
+        {
+            _evaluate.Orders.Add($"'{_evaluate.Table}'[{expression}] DESC");
+
+            return this;
+        }
+
+        public EvaluateBuilder StartAt(string value)
+        {
+            _evaluate.StartAts.Add($"\"{value}\"");
+
+            return this;
+        }
+
+        public EvaluateBuilder StartAt(int value)
+        {
+            _evaluate.StartAts.Add(value.ToString());
+
+            return this;
+        }
+    }
+
+    public class EvaluateQuery
+    {
+        public string Table { get; set; }
+
+        public List<string> Orders { get; set; }
+
+        public List<string> StartAts { get; set; }
+    }
+}
---
+++ tests/Builder.Tests/UnitTest1.cs
@@ -0,0 +1,47 @@
+using Xunit;
+
+namespace Builder.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Evaluate_With_Multiple_Orders_Test()
+        {
+            QueryBuilder builder = new();
+
+            string actualQuery = builder.Evaluate(x =>
+                               {
+                                   x.Table("TestTable1")
+                                    .OrderBy("Col1")
+                                    .OrderByDescending("Col2 With Space");
+                               })
+                               .BuildRaw();
+
+            string expectedQuery = $@"EVALUATE('TestTable1') ORDER BY 'TestTable1'[Col1] ASC, 'TestTable1'[Col2 With Space] DESC";
+
+            Assert.Equal(expectedQuery, actualQuery, ignoreWhiteSpaceDifferences: true);
+
+        }
+
+        [Fact]
+        public void Evaluate_With_Multiple_Orders_And_StartAts_Test()
+        {
+            QueryBuilder builder = new();
+
+            string actualQuery = builder.Evaluate(x =>
+            {
+                x.Table("TestTable1")
+                 .OrderBy("Col1")
+                 .OrderByDescending("Col2 With Space")
+                 .StartAt(50)
+                 .StartAt("50");
+            })
+                .BuildRaw();
+
+            string expectedQuery = $"EVALUATE('TestTable1') ORDER BY 'TestTable1'[Col1] ASC, 'TestTable1'[Col2 With Space] DESC START AT 50, \"50\"";
+
+            Assert.Equal(expectedQuery, actualQuery, ignoreWhiteSpaceDifferences: true);
+
+        }
+    }
+}
